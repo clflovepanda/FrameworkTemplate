8799298332658c6bf32e6fe426798b4d989ae34f
clflovepanda <982294498@qq.com>
2020-02-23 13:30:35 +0800

    init bootstrap ssm

--- src/main/java/com/panda/template/Main.java
+++ src/main/java/com/panda/template/Main.java
@@ -1,4 +1,12 @@
 package com.panda.template;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.panda.template")
+@MapperScan("com.panda.template.dao")
 public class Main {
+
 }
