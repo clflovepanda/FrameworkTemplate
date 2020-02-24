b72e5206ea8edfd429e2165da4f0fc7b960f8a9f
clflovepanda <982294498@qq.com>
2020-02-24 23:25:05 +0800

    init ssm velocity

--- src/main/java/com/panda/template/service/StudentService.java
+++ src/main/java/com/panda/template/service/StudentService.java
@@ -3,15 +3,20 @@ package com.panda.template.service;
 import com.panda.template.dao.StudentDao;
 import com.panda.template.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 @Service
 public class StudentService {
 
+    @PostConstruct
+    public void init() {
+
+    }
+
     @Autowired
     private StudentDao studentDao;
 
-    public Student getStudent(int id) {
-        return studentDao.getById(id);
-    }
 }
