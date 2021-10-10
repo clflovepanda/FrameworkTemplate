f6cffe5ac8142de8da078218bd7a009a59abc2d3
changlifeng <panda15846504013>
2021-10-10 19:47:10 +0800

    spring boot init

--- src/main/java/com/panda/template/Main.java
+++ src/main/java/com/panda/template/Main.java
@@ -1,12 +1,16 @@
 package com.panda.template;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.panda.template")
+@ImportResource("classpath:conf/database.xml")
 @MapperScan("com.panda.template.dao")
 public class Main {
 
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
--- src/main/java/com/panda/template/dao/StudentDao.java
+++ src/main/java/com/panda/template/dao/StudentDao.java
@@ -2,8 +2,6 @@ package com.panda.template.dao;
 
 import com.panda.template.entity.Student;
 
-import java.util.List;
-
 public interface StudentDao {
 
     Student getById(int id);
